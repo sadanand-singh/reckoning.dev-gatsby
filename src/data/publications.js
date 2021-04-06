daf5c26ddbf64a3bdf6f9f44dcaf3e8e87bc9081
Sadanand Singh <sadanand4singh@gmail.com>
2021-04-05 21:39:24 -0700

    fix links in publications

--- src/data/publications.js
+++ src/data/publications.js
@@ -19,6 +19,32 @@ const data = [
                 pdf: "https://www.spiedigitallibrary.org/conference-proceedings-of-spie/11597/115970W/An-improved-mammography-malignancy-model-with-self-supervised-learning/10.1117/12.2582318.full?SSO=1",
                 arxiv: "https://arxiv.org/abs/2001.08381"
             },
+            {
+                title: "Deep Learning / Machine Learning test 2",
+                authors: [
+                    {
+                        name: "Sadanand Singh",
+                        primary: true
+                    },
+                ],
+                label: "SPIE 2020",
+                link: "https://spie.org/MI/conferencedetails/computer-aided-diagnosis#session-1",
+                pdf: "https://www.spiedigitallibrary.org/conference-proceedings-of-spie/11597/115970W/An-improved-mammography-malignancy-model-with-self-supervised-learning/10.1117/12.2582318.full?SSO=1",
+                arxiv: "https://arxiv.org/abs/2001.08381"
+            },
+            {
+                title: "Deep Learning / Machine Learning test 3",
+                authors: [
+                    {
+                        name: "Sadanand Singh",
+                        primary: true
+                    },
+                ],
+                label: "SPIE 2020-II",
+                link: "https://spie.org/MI/conferencedetails/computer-aided-diagnosis#session-1",
+                pdf: "https://www.spiedigitallibrary.org/conference-proceedings-of-spie/11597/115970W/An-improved-mammography-malignancy-model-with-self-supervised-learning/10.1117/12.2582318.full?SSO=1",
+                arxiv: "https://arxiv.org/abs/2001.08381"
+            },
         ],
     }
 ]
--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import { ExtLink, InlinePageLink } from "../components/atoms"
 import Contact from "../components/contact"
 import publications from "../data/publications"
 
-const PdfLink = (link) => {
+const PdfLink = ({ link }) => {
   return (
     <a href={link} target="_blank" rel="noopener noreferrer" className="mr-2 text-accent  hover:text-secondary">
       <span><svg aria-hidden="true" className="mr-1 inline h-4 w-4 transition duration-100 " fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12.819 14.427c.064.267.077.679-.021.948-.128.351-.381.528-.754.528h-.637v-2.12h.496c.474 0 .803.173.916.644zm3.091-8.65c2.047-.479 4.805.279 6.09 1.179-1.494-1.997-5.23-5.708-7.432-6.882 1.157 1.168 1.563 4.235 1.342 5.703zm-7.457 7.955h-.546v.943h.546c.235 0 .467-.027.576-.227.067-.123.067-.366 0-.489-.109-.198-.341-.227-.576-.227zm13.547-2.732v13h-20v-24h8.409c4.858 0 3.334 8 3.334 8 3.011-.745 8.257-.42 8.257 3zm-12.108 2.761c-.16-.484-.606-.761-1.224-.761h-1.668v3.686h.907v-1.277h.761c.619 0 1.064-.277 1.224-.763.094-.292.094-.597 0-.885zm3.407-.303c-.297-.299-.711-.458-1.199-.458h-1.599v3.686h1.599c.537 0 .961-.181 1.262-.535.554-.659.586-2.035-.063-2.693zm3.701-.458h-2.628v3.686h.907v-1.472h1.49v-.732h-1.49v-.698h1.721v-.784z"></path></svg></span>
@@ -14,7 +14,7 @@ const PdfLink = (link) => {
   )
 }
 
-const ArxivLink = (link) => {
+const ArxivLink = ({ link }) => {
   return (
     <a href={link} target="_blank" rel="noopener noreferrer" class="mr-2 text-accent  hover:text-secondary">
       <span><svg aria-hidden="true" class="inline h-4 w-4 mr-1  transition duration-100" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M6 17c2.269-9.881 11-11.667 11-11.667v-3.333l7 6.637-7 6.696v-3.333s-6.17-.171-11 5zm12 .145v2.855h-16v-12h6.598c.768-.787 1.561-1.449 2.339-2h-10.937v16h20v-6.769l-2 1.914z"></path></svg></span>
@@ -23,7 +23,7 @@ const ArxivLink = (link) => {
   )
 }
 
-const PatentLink = (link) => {
+const PatentLink = ({ link }) => {
   return (
     <a href={link} target="_blank" rel="noopener noreferrer" class="mr-2 text-accent  hover:text-secondary">
       <span><svg aria-hidden="true" class="inline h-4 w-4 mr-1  transition duration-100" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M6 17c2.269-9.881 11-11.667 11-11.667v-3.333l7 6.637-7 6.696v-3.333s-6.17-.171-11 5zm12 .145v2.855h-16v-12h6.598c.768-.787 1.561-1.449 2.339-2h-10.937v16h20v-6.769l-2 1.914z"></path></svg></span>
