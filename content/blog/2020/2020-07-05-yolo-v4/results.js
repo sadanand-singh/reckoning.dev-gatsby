fde57d34794dc72dd5caa543f2611e8df509b11d
Sadanand Singh <sadanand4singh@gmail.com>
2021-04-08 15:52:19 -0700

    add table of results to YOLO post

---
+++ content/blog/2020/2020-07-05-yolo-v4/results.js
@@ -0,0 +1,220 @@
+const data = [
+    {
+        primary_key: "Model",
+        results_keys: ['Size', 'FPS', 'AP', 'AP_small']
+    },
+    {
+        results: [
+            {
+                Model: 'YOLOv4',
+                'Size': 416,
+                'AP_small': 20.4,
+                'AP': 41.2,
+                'FPS': 38
+            },
+            {
+                Model: 'YOLOv4',
+                'Size': 512,
+                'AP_small': 24.3,
+                'AP': 43.0,
+                'FPS': 31
+            },
+            {
+                Model: 'YOLOv4',
+                'Size': 608,
+                'AP_small': 26.7,
+                'AP': 43.5,
+                'FPS': 23
+            },
+            {
+                Model: 'YOLOv3',
+                'Size': 320,
+                'AP_small': 11.9,
+                'AP': 28.2,
+                'FPS': 45
+            },
+            {
+                Model: 'YOLOv3',
+                'Size': 416,
+                'AP_small': 15.2,
+                'AP': 31.0,
+                'FPS': 35
+            },
+            {
+                Model: 'YOLOv3',
+                'Size': 608,
+                'AP_small': 18.3,
+                'AP': 33.0,
+                'FPS': 20
+            },
+            {
+                Model: 'YOLOv3-SPP',
+                'Size': 608,
+                'AP_small': 20.6,
+                'AP': 36.2,
+                'FPS': 20
+            },
+        ]
+    },
+    {
+        results: [
+            {
+                Model: 'LRF-VGG-16',
+                'Size': 300,
+                'AP_small': 12.6,
+                'AP': 32.0,
+                'FPS': 76.9
+            },
+            {
+                Model: 'LRF-ResNet-101',
+                'Size': 300,
+                'AP_small': 13.2,
+                'AP': 34.2,
+                'FPS': 52.6
+            },
+            {
+                Model: 'LRF-VGG-16',
+                'Size': 512,
+                'AP_small': 19.0,
+                'AP': 36.2,
+                'FPS': 38.5
+            },
+            {
+                Model: 'LRF-ResNet-101',
+                'Size': 512,
+                'AP_small': 19.7,
+                'AP': 37.3,
+                'FPS': 31.3
+            },
+            {
+                Model: 'RFBNet',
+                'Size': 300,
+                'AP_small': 11.8,
+                'AP': 30.3,
+                'FPS': 66.7
+            },
+            {
+                Model: 'RFBNet',
+                'Size': 512,
+                'AP_small': 16.2,
+                'AP': 33.8,
+                'FPS': 33.3
+            },
+            {
+                Model: 'RFBNet-E',
+                'Size': 512,
+                'AP_small': 17.6,
+                'AP': 34.4,
+                'FPS': 30.3
+            },
+        ]
+    },
+    {
+        results: [
+            {
+                Model: 'M2det-VGG-16',
+                'Size': 320,
+                'AP_small': 14.4,
+                'AP': 33.5,
+                'FPS': 33.4
+            },
+            {
+                Model: 'M2det-Resnet101',
+                'Size': 320,
+                'AP_small': 14.8,
+                'AP': 34.3,
+                'FPS': 21.7
+            },
+            {
+                Model: 'M2det-VGG-16',
+                'Size': 512,
+                'AP_small': 18.4,
+                'AP': 37.6,
+                'FPS': 18.0
+            },
+            {
+                Model: 'M2det-Resnet101',
+                'Size': 512,
+                'AP_small': 20.5,
+                'AP': 38.8,
+                'FPS': 15.8
+            },
+            {
+                Model: 'M2det-VGG-16',
+                'Size': 800,
+                'AP_small': 22.1,
+                'AP': 41.0,
+                'FPS': 11.8
+            },
+            {
+                Model: 'RetinaNet-Resnet-50',
+                'Size': 500,
+                'AP_small': 13.9,
+                'AP': 32.5,
+                'FPS': 13.9
+            },
+            {
+                Model: 'RetinaNet-Resnet-101',
+                'Size': 500,
+                'AP_small': 14.7,
+                'AP': 34.4,
+                'FPS': 11.1
+            },
+            {
+                Model: 'RetinaNet-Resnet-50',
+                'Size': 800,
+                'AP_small': 18.9,
+                'AP': 35.7,
+                'FPS': 6.5
+            },
+            {
+                Model: 'RetinaNet-Resnet-101',
+                'Size': 800,
+                'AP_small': 20.2,
+                'AP': 37.8,
+                'FPS': 5.1
+            },
+        ]
+    },
+    {
+        results: [
+            {
+                Model: 'SSD',
+                'Size': 320,
+                'AP_small': 6.6,
+                'AP': 25.1,
+                'FPS': 43.0
+            },
+            {
+                Model: 'SSD',
+                'Size': 512,
+                'AP_small': 10.9,
+                'AP': 28.8,
+                'FPS': 22.0
+            },
+            {
+                Model: 'RefineDet',
+                'Size': 320,
+                'AP_small': 10.0,
+                'AP': 29.4,
+                'FPS': 38.7
+            },
+            {
+                Model: 'RefineDet',
+                'Size': 512,
+                'AP_small': 16.3,
+                'AP': 33.0,
+                'FPS': 22.3
+            },
+            {
+                Model: 'CornerNet-Hourglass',
+                'Size': 512,
+                'AP_small': 20.8,
+                'AP': 40.5,
+                'FPS': 4.4
+            },
+        ]
+    },
+]
+
+export default data
--- src/components/table.js
+++ src/components/table.js
@@ -3,6 +3,7 @@ import React from "react"
 const ResultsTable = ({ data, id }) => {
     const headingData = data[0]
     const results = data.slice(1)
+    const num_keys = results.length
     return (
         <div id={id} className="mb10 block mb-4 bg-white rounded">
             <div className="text-sm divide-y border p-2 border-opacity-50 border-dashed rounded">
@@ -10,7 +11,7 @@ const ResultsTable = ({ data, id }) => {
                     <div className="text-sm w-48 md:w-64 mr-16 md:mr-3 pb-1">
                         {headingData.primary_key}
                     </div>
-                    <div className="flex-grow grid grid-cols-3">
+                    <div className={`flex-grow grid grid-cols-${num_keys}`}>
                         {headingData.results_keys.map((tag, index) => (
                             <span key={index} className="font-semibold">
                                 {tag}
@@ -22,9 +23,9 @@ const ResultsTable = ({ data, id }) => {
                     <div key={index} className="flex flex-row">
                         <div className="h-32 overflow-y-scroll flex-grow ">
                             {res_data.results.map((res_model_data, index) => (
-                                <div className="flex flex-row">
+                                <div key={index} className="flex flex-row">
                                     <div className="text-sm w-48 md:w-64 mr-3">{res_model_data[headingData.primary_key]}</div>
-                                    <div className="flex-grow grid grid-cols-3">
+                                    <div className={`flex-grow grid grid-cols-${num_keys}`}>
                                         {headingData.results_keys.map((k, index) => (
                                             <span key={index} className="font-semibold">
                                                 {(res_model_data[k] !== undefined ? res_model_data[k] : '--')}
